52a5e31d03acad9d1235e0e91b2f8083133b20d9
Arpit <arpitwashal30@gmail.com>
2023-05-22 21:59:10 +0530

    code pused

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+const developement = {
+    name :'developement',
+    db_path :'./config/mongoose',
+    passport_path : './config/passport-local-strategy',
+    customMware_path : './config/flashMessage',
+    assets_path : './assets',
+    secret_key :'#placementCell@careercamp$$Team&&&interface##Amit^^Singh%%',
+    mongoose_path :'mongodb+srv://arpitwashal30:0SwNWNKZN7bgUMFe@cluster0.yyo8frq.mongodb.net/?retryWrites=true&w=majority',
+    api_path :'https://remotive.com/api/remote-jobs'
+}
+module.exports = developement;
